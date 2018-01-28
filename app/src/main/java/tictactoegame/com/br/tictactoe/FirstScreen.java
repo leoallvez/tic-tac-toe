5b26d3ea10ae677e9e1a8d456ca098cb065c7fbf
leoallvez <leoallvez@hotmail.com>
2018-01-28 02:49:35 -0200

    prototype finish

---
+++ app/src/main/java/tictactoegame/com/br/tictactoe/FirstScreen.java
@@ -0,0 +1,49 @@
+package tictactoegame.com.br.tictactoe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import tictactoegame.com.br.tictactoe.MainActivity;
+import tictactoegame.com.br.tictactoe.R;
+
+public class FirstScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+
+        final EditText etPlayer1 = (EditText) findViewById(R.id.etPlayer1);
+        final EditText etPlayer2 = (EditText) findViewById(R.id.etPlayer2);
+
+        Button bPlay = (Button) findViewById(R.id.bPlay);
+
+        bPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Getting strings from edit text
+                String PLAYER_1 = "" + etPlayer1.getText().toString();
+                String PLAYER_2 = "" + etPlayer2.getText().toString();
+
+                //Intents are used to shift from one activity to another.
+                //We can also transfer data with intents.
+
+                //Creating an object of intent through which we can go from current activity to MainAcivity
+                Intent i = new Intent(FirstScreen.this,MainActivity.class);
+
+                //Adding data with intent to transfer.
+                i.putExtra("PLAYER_1",PLAYER_1);
+                i.putExtra("PLAYER_2",PLAYER_2);
+
+                //Calling to intent to open MainActivity.
+                startActivity(i);
+
+            }
+        });
+    }
+}
