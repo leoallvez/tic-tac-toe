9b2407061fda3d97e3a7eeac0fbf58cb5c62818a
leoallvez <leoallvez@hotmail.com>
2018-02-01 02:00:11 -0200

    player vs machine - prototype

--- app/src/main/java/tictactoegame/com/br/code/activity/OnePlayerActivity.java
+++ app/src/main/java/tictactoegame/com/br/code/activity/OnePlayerActivity.java
@@ -1,36 +1,50 @@
 package tictactoegame.com.br.code.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
+import android.os.Bundle;
+import android.widget.Button;
 import tictactoegame.com.br.code.R;
 import tictactoegame.com.br.code.model.Board;
 import tictactoegame.com.br.code.model.Player;
+import android.support.v7.app.AppCompatActivity;
 import tictactoegame.com.br.code.model.PlayerVirtual;
 
 public class OnePlayerActivity extends AppCompatActivity {
 
-    private PlayerVirtual playerVirtual;
-    private Player player;
     private Board board;
+    private Player player;
+    private PlayerVirtual playerVirtual;
+    private int[] buttonsId = {
+        R.id.btn00, R.id.btn01, R.id.btn02,
+        R.id.btn03, R.id.btn04, R.id.btn05,
+        R.id.btn06, R.id.btn07, R.id.btn08
+    };
+    private Button[] buttons = new Button[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_player);
         initialize();
+        startElements();
+    }
+
+    private void startElements() {
+
+        for(int i = 0; i < buttons.length; i++){
+            buttons[i] = (Button) findViewById(buttonsId[i]);
+        }
     }
 
     private void initialize(){
         playerVirtual = new PlayerVirtual("Machine",0);
         player = new Player ("You",1);
         board = new Board(playerVirtual,player);
+        board.start();
     }
 
     public void onClick(View v) {
-        //Toast.makeText(this, v.getTag().toString(), Toast.LENGTH_LONG).show();
+
         played(Integer.parseInt(v.getTag().toString()));
     }
 
@@ -42,10 +56,34 @@ public class OnePlayerActivity extends AppCompatActivity {
             if (!playerVirtual.won()) {
                 playerVirtual.analyzeAndPlay(playerVirtual.getId());
             }
-            //preencheLacunas();
+            fills();
             board.show();
             if (board.gameOver()) {
-                //mostraResultado();
+                //showResult();
+            }
+        }
+    }
+
+    public void newGame(View v){
+        board.start();
+        fills();
+        player.turnChange();
+        if(playerVirtual.getTurn()){playerVirtual.randomPlay();}
+    }
+
+    private void fills(){
+        int[]a = Board.positions;
+        for(int i = 0; i < a.length; i++){
+
+            if(board.showPosition(i) == player.getId()){
+                buttons[i].setBackgroundColor(getResources().getColor(R.color.lightBlue));
+                buttons[i].setText("X");
+            }else if (board.showPosition(i) == playerVirtual.getId()){
+                buttons[i].setBackgroundColor(getResources().getColor(R.color.colorPlayerVirtal));
+                buttons[i].setText("O");
+            }else{
+                buttons[i].setBackgroundColor(getResources().getColor(R.color.colorGreen));
+                buttons[i].setText("");
             }
         }
     }
