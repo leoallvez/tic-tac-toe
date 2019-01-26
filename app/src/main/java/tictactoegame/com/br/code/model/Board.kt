2a2e0b9467f36e16b567232cf3098c1d741bd99c
Leonardo Alves <leoallvez@hotmail.com>
2019-01-25 22:53:54 -0300

    Convertendo códigos java em kotlin

---
+++ app/src/main/java/tictactoegame/com/br/code/model/Board.kt
@@ -0,0 +1,43 @@
+package tictactoegame.com.br.code.model
+
+/*
+ Created by Leonardo on 25/01/2019.
+*/
+class Board(playerOne:Player, playerTwo:Player) {
+
+    val playerOne:Player
+    val playerTwo:Player
+    val isFullyPopulated:Boolean
+
+        get() {
+            var count = 0
+            for (position in positions) {
+                if (position == playerOne.tag || position == playerTwo.tag) {
+                    count++
+                }
+            }
+            return count == Board.positions.size
+        }
+
+    init{
+        this.playerOne = playerOne
+        this.playerTwo = playerTwo
+        start()
+    }
+
+    fun start() {
+        positions = intArrayOf(3, 2, 3, 2, 4, 2, 3, 2, 3)
+    }
+
+    fun showPosition(movement:Int):Int {
+        return positions[movement]
+    }
+
+    fun gameOver()= isFullyPopulated || playerOne.won() || playerTwo.won()
+
+    companion object {
+        var positions = IntArray(9)
+        val minimum = 2
+        fun isEmptyPosition(movement:Int)= positions[movement] >= minimum
+    }
+}
