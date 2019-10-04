b8d651953171f9f81d13d005cff70641ea70d939
leoallvez <leoallvez@hotmail.com>
2019-10-03 23:57:43 -0300

    algoritmo invencivel

--- app/src/main/java/tictactoegame/com/br/code/model/PlayerVirtual.kt
+++ app/src/main/java/tictactoegame/com/br/code/model/PlayerVirtual.kt
@@ -122,7 +122,7 @@ class PlayerVirtual(name: String = "Machine", tag: Int = 0) : Player(name, tag) 
             return true
         }
 
-        if (positions[6] == tag && positions[2] == tag && Board.isEmptyPosition(4)) {
+        if (positions[Position.fromInt(6).number] == tag && positions[2] == tag && Board.isEmptyPosition(4)) {
             positions[4] = brand
             return true
         }
@@ -131,6 +131,19 @@ class PlayerVirtual(name: String = "Machine", tag: Int = 0) : Player(name, tag) 
 
         return false
     }
+    // 1 - se o oponente ocupar duas casas seguidas, ocupe a terceira
+    // 2 - caso contrário se algum movimento que cria duas linhar com
+    // duas casas ocupadas, faça ele.
+    // 3 - caso contrário, se o espaço do centro estive vazio, ocupe ele
+    fun centerIsEmpty(): VictoryAnalysis{
+        val position = Position.CENTER_CENTER.number
+        return VictoryAnalysis(Board.isEmptyPosition(position), position )
+    }
+    // 4 - caso contrário, se o oponente preencheu uma quina, preencha a
+    // quina contrária.
+    // 5 - caso contrário, se tiver uma quina vázia preencha ela.
+    // 6 - se nenhuma dessas condições acontencer, pode preencher qualquer
+    // espaço vázio.
 
     fun analyzeAndPlay(opponentTag: Int) {
         if (canWin(tag).not()) { canWin(opponentTag, true) }
@@ -140,4 +153,6 @@ class PlayerVirtual(name: String = "Machine", tag: Int = 0) : Player(name, tag) 
     companion object {
         private val brand = 10
     }
+
+    inner class VictoryAnalysis(val win: Boolean, val position: Int)
 }
