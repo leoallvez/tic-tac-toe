e0a337a331ec70cd4c7f7660317cb7817b3decbb
Leonardo <leoallvez@hotmail.com>
2021-03-13 02:43:34 -0300

    finalização da arquitetura mvvm

--- app/src/main/java/io/github/leoallvez/tictactoe/OnePlayerFragment.kt
+++ app/src/main/java/io/github/leoallvez/tictactoe/OnePlayerFragment.kt
@@ -1,91 +1,51 @@
 package io.github.leoallvez.tictactoe
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
 import io.github.leoallvez.tictactoe.databinding.FragmentOnePlayerBinding
-import io.github.leoallvez.tictactoe.model.Board
-import io.github.leoallvez.tictactoe.model.HumanPlayer
-import io.github.leoallvez.tictactoe.model.VirtualPlayer
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OnePlayerFragment : Fragment() {
 
     private val viewModel: OnePlayerViewModel by viewModel()
-
-    private val humanPlayer = HumanPlayer(Seed.O)
-    private val virtualPlayer = VirtualPlayer(Seed.X)
-    private val board = Board
-    private val buttons = mutableListOf<Button>()
     private lateinit var binding: FragmentOnePlayerBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentOnePlayerBinding.inflate(inflater, container, false)
-        startElements(binding.root)
-        binding.viewModel = this
+        startElements()
         return binding.root
     }
 
-    private fun startElements(view: View) {
-
-        board.run { row, col ->
-            val button = view.findViewWithTag<Button>("$row:$col")
-            button.setOnClickListener { played(row, col) }
-            buttons.add(button)
-        }
-        startGame()
-    }
-
-    fun played(row: Int, col: Int) {
-
-        if (notGameOver(row, col)) {
-            humanPlayer.play(row, col)
-            if (humanPlayer.won()) { humanPlayer.toScore() }
-            if (notGameOver(row, col).not()) {
-                if (virtualPlayer.won().not()) {
-                    virtualPlayer.play()
-                    if (virtualPlayer.won()) {
-                        virtualPlayer.toScore()
-                    }
+    private fun startElements() {
+        binding.viewModel = viewModel
+        viewModel.startGame()
+        viewModel.getBoard().observe(viewLifecycleOwner, { board ->
+            board.run { row, col ->
+                when(board.cells[row][col].content) {
+                    Seed.O -> print(row, col, R.color.lightBlue, "X")
+                    Seed.X -> print(row, col, R.color.lightPink, "0")
+                    else   -> print(row, col, R.color.colorGreen, null)
                 }
             }
-            fills()
-        }
-    }
-
-    private fun notGameOver(row: Int, col: Int): Boolean {
-        val isEmptyPosition = Board.cells[row][col].isEmpty()
-        val someOneWon = humanPlayer.won() || virtualPlayer.won()
-        return isEmptyPosition && someOneWon.not()
-    }
-
-    fun startGame() {
-        board.restart()
-        virtualPlayer.randomPlay()
-        fills()
-    }
-
-    private fun fills() {
-        //points.text = getString(R.string.score, humanPlayer.points, virtualPlayer.points)
-        board.run { row, col ->
-            when(board.cells[row][col].content) {
-                Seed.O -> print(row, col, R.color.lightBlue, "X")
-                Seed.X -> print(row, col, R.color.lightPink, "0")
-                else   -> print(row, col, R.color.colorGreen, null)
+        })
+        viewModel.getPoints().observe(viewLifecycleOwner, { points ->
+            points?.let {
+                binding.points.text = points
             }
-        }
+        })
     }
 
     private fun print(row: Int, col: Int, colorId: Int, symbol: String?) {
         val color = ResourcesCompat.getColor(resources, colorId, null)
-        val button = buttons.find { it.tag == "$row:$col" }
+        val button = binding.root.findViewWithTag<Button>("$row:$col")
         button?.let {
             it.setBackgroundColor(color)
             it.text = symbol
--- app/src/main/java/io/github/leoallvez/tictactoe/OnePlayerViewModel.kt
+++ app/src/main/java/io/github/leoallvez/tictactoe/OnePlayerViewModel.kt
@@ -1,7 +1,54 @@
 package io.github.leoallvez.tictactoe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.github.leoallvez.tictactoe.model.Board
+import io.github.leoallvez.tictactoe.model.HumanPlayer
+import io.github.leoallvez.tictactoe.model.VirtualPlayer
 
 class OnePlayerViewModel : ViewModel() {
 
+    private val humanPlayer = HumanPlayer(Seed.O)
+    private val virtualPlayer = VirtualPlayer(Seed.X)
+    private val points: MutableLiveData<String> = MutableLiveData(
+        "Me: 0, Machine: 0"
+    )
+    fun getPoints(): LiveData<String> = points
+    private val _board = Board
+    private val board: MutableLiveData<Board> = MutableLiveData(_board)
+    fun getBoard(): LiveData<Board> = board
+
+    private fun updateLiveData() {
+        board.value = _board
+        points.value = "Me: ${humanPlayer.points}, Machine: ${virtualPlayer.points}"
+    }
+
+    fun play(row: Int, col: Int) {
+        if (notGameOver(row, col)) {
+            humanPlayer.play(row, col)
+            if (humanPlayer.won()) { humanPlayer.toScore() }
+            if (notGameOver(row, col).not()) {
+                if (virtualPlayer.won().not()) {
+                    virtualPlayer.play()
+                    if (virtualPlayer.won()) {
+                        virtualPlayer.toScore()
+                    }
+                }
+            }
+            updateLiveData()
+        }
+    }
+
+    private fun notGameOver(row: Int, col: Int): Boolean {
+        val isEmptyPosition = _board.cells[row][col].isEmpty()
+        val someOneWon = humanPlayer.won() || virtualPlayer.won()
+        return isEmptyPosition && someOneWon.not()
+    }
+
+    fun startGame() {
+        _board.restart()
+        virtualPlayer.randomPlay()
+        updateLiveData()
+    }
 }
